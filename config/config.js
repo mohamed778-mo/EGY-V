41fa44739c4889b4597da94155ede153924428b5
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-07-12 02:40:20 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose")
 require("dotenv").config()
 const URL =process.env.MONGODB_URL
 const connection = ()=>{
-   mongoose.connect('mongodb://127.0.0.1:27017/EGY-V')
+   mongoose.connect('mongodb+srv://muhammadelmalla13:vqwTQAACDU6v0FGO@cluster0.njnkmyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
       .then(()=>{console.log('done connection !!')})
   .catch((error)=>{console.log(error.message)})
 }
