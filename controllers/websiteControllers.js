fdccb1822abc9cf4f3c66a4790aa2da2f43688f4
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-07-12 19:17:12 +0300

    Update websiteControllers.js

--- controllers/websiteControllers.js
+++ controllers/websiteControllers.js
@@ -202,7 +202,8 @@ exports.login_user = async (req, res) => {
     const token = jwt.sign({ id: user._id, type: user.role }, SECRETKEY,{ expiresIn: '7d' }) ;
     res.cookie("access_token", `Bearer ${token}`, {
       maxAge: 7 * 24 * 60 * 60 * 1000, 
-      secure: true, 
+      secure: false,      
+      sameSite: 'Lax',  
       httpOnly: true,
     });
     user.tokens.push(token);
