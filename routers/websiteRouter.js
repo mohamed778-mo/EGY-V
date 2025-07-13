ed8c4b4ed7762205b017e38829648e3ca7267e29
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-07-13 03:29:02 +0300

    Update websiteRouter.js

--- routers/websiteRouter.js
+++ routers/websiteRouter.js
@@ -11,9 +11,9 @@ router.post('/verify-otp', websiteControllers.verify);
 router.post('/resend-otp', websiteControllers.resend_otp);
 router.post('/login', websiteControllers.login_user);
 
-router.get('/filter-trips', websiteControllers.filterTrips);
-router.get('/get_all_trips', websiteControllers.getAllTrips)
-router.get('/get_trip/:id', websiteControllers.getTrip)
+router.get('/filter-trips',auth , websiteControllers.filterTrips);
+router.get('/get_all_trips', auth , websiteControllers.getAllTrips)
+router.get('/get_trip/:id',auth, websiteControllers.getTrip)
 
 router.post('/create-booking/:id',auth, websiteControllers.createBooking);
 
