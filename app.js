d0fde69482e56af923b8ebddabf3b840daf0cc7e
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-07-13 04:28:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use(helmet());
 
 app.use(cors({
   origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
